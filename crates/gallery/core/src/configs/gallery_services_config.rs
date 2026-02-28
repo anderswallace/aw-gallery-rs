82ad29aca53059f271265a0b1a21961d26b1e19d
Anders Wallace <anderswallace@proton.me>
2026-02-28 02:35:59 -0600

    feat: create services config struct

--- crates/gallery/core/src/configs/gallery_services_config.rs
+++ crates/gallery/core/src/configs/gallery_services_config.rs
@@ -4,7 +4,8 @@ use serde::{Deserialize, Serialize};
 use crate::configs::services::gallery_s3_objects_service_impl::GalleryS3ObjectsServiceImpl;
 
 /// Configuration that selects the implementation for each Gallery service used by Gallery
-#[derive(Debug, Clone, PartialEq, PartialOrd, Deserialize, Serialize, Eq, JsonSchema)]
-pub struct GalleryS3ServicesConfig {
-    pub s3_object: GalleryS3ObjectsServiceImpl,
+#[derive(Debug, Clone, PartialEq, PartialOrd, Deserialize, Serialize, Eq, JsonSchema, Default)]
+#[serde(rename_all = "camelCase", deny_unknown_fields)]
+pub struct GalleryServicesConfig {
+    pub s3_objects: GalleryS3ObjectsServiceImpl,
 }
