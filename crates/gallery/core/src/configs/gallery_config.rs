5b1a440abbc5bfd1507b9f997b832beb9016fb26
Anders Wallace <anderswallace@proton.me>
2026-02-28 02:51:46 -0600

    feat: implement Default trait for GalleryConfig

--- crates/gallery/core/src/configs/gallery_config.rs
+++ crates/gallery/core/src/configs/gallery_config.rs
@@ -6,10 +6,19 @@ use crate::configs::gallery_services_config::GalleryServicesConfig;
 
 /// Root configuration for the Gallery core
 #[derive(
-    Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash, Deserialize, Serialize, Default, JsonSchema,
+    Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash, Deserialize, Serialize, JsonSchema,
 )]
 #[serde(rename = "camelCase", deny_unknown_fields)]
 pub struct GalleryConfig {
     pub io: GalleryIoConfig,
     pub services: GalleryServicesConfig,
+}
+
+impl Default for GalleryConfig {
+    fn default() -> Self {
+        Self {
+            io: GalleryIoConfig::default(),
+            services: GalleryServicesConfig::default(),
+        }
+    }
 }
