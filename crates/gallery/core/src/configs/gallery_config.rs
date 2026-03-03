e644e320820563eceeb3fac84659c71a85aaee71
Anders Wallace <anderswallace@proton.me>
2026-03-03 17:37:28 -0600

    refactor: derive Default trait for GalleryConfig, remove aws credentials from S3Config

--- crates/gallery/core/src/configs/gallery_config.rs
+++ crates/gallery/core/src/configs/gallery_config.rs
@@ -6,19 +6,10 @@ use crate::configs::gallery_services_config::GalleryServicesConfig;
 
 /// Root configuration for the Gallery core
 #[derive(
-    Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash, Deserialize, Serialize, JsonSchema,
+    Debug, Default, Clone, PartialEq, Eq, PartialOrd, Ord, Hash, Deserialize, Serialize, JsonSchema,
 )]
 #[serde(rename = "camelCase", deny_unknown_fields)]
 pub struct GalleryConfig {
     pub io: GalleryIoConfig,
     pub services: GalleryServicesConfig,
-}
-
-impl Default for GalleryConfig {
-    fn default() -> Self {
-        Self {
-            io: GalleryIoConfig::default(),
-            services: GalleryServicesConfig::default(),
-        }
-    }
 }
--- crates/gallery/core/src/configs/s3_config.rs
+++ crates/gallery/core/src/configs/s3_config.rs
@@ -1,15 +1,28 @@
+use anyhow::{Result, bail};
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
-//TODO: Set struct values to newtypes when we start implementing the IO
+// TODO: Set struct values to newtypes when we start implementing the IO
 /// Configuration for interacting with AWS bucket
 #[derive(
-    Debug, Clone, PartialEq, PartialOrd, Ord, Eq, Hash, Serialize, Deserialize, JsonSchema, Default,
+    Debug, Default, Clone, PartialEq, PartialOrd, Ord, Eq, Hash, Serialize, Deserialize, JsonSchema,
 )]
-#[serde(rename = "camelCase", deny_unknown_fields)]
+#[serde(rename_all = "camelCase", deny_unknown_fields)]
 pub struct S3Config {
-    pub access_key: String,
-    pub secret_access_key: String,
     pub region: String,
     pub bucket: String,
+}
+
+impl S3Config {
+    fn validate_field(&self) -> Result<()> {
+        if self.region.is_empty() {
+            bail!("Region is not set for S3Config")
+        }
+
+        if self.bucket.is_empty() {
+            bail!("Bucket is not set for S3Config")
+        }
+
+        Ok(())
+    }
 }
